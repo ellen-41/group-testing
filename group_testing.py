e1c143310bd006d28c9d960d62eaf20b4c3742d1
Ellen Brown <ellen.brown41@gmail.com>
2019-11-11 12:45:36 +0000

    Initial commit

---
+++ group_testing.py
@@ -0,0 +1,75 @@
+import numpy as np
+import random
+
+
+def generate_bin_array(n, p):
+    '''Generate a random array of 1s and 0s, 1s appearing with probability p'''
+    bin_array = []
+    for i in range(1, n + 1):
+        value = np.random.binomial(1, p)
+        bin_array.append(value)
+    return bin_array
+
+
+class TestSet:
+    '''Object representing a test set of size n with roughly k defectives'''
+    def __init__(self, n, k):
+        self.n = n
+        self.k = k
+        self.defective_prob = k/n
+        self.create_array()
+
+    def create_array(self):
+        '''Generates the test set as an array of 1s and 0s based on the
+            probability of an item being defective being k/n'''
+        self.test_array = generate_bin_array(self.n, self.defective_prob)
+
+
+class Algorithm:
+    '''Represents a generic algorithm'''
+    def __init__(self, TestSet):
+        self.test_array = TestSet.test_array
+        self.n = TestSet.n
+        self.choose_group()
+
+    def choose_group(self):
+        '''Randomly generates a group to test'''
+        self.group = generate_bin_array(self.n, 0.5)
+        # What should this probability be? Random?
+        print(self.group)
+
+
+class SSS(Algorithm):
+    '''Smallest satisfying set'''
+    def __init__(self, TestSet):
+        super().__init__(TestSet)
+
+    def test_group(self):
+        self.members_tested = []
+        for i in range(0, self.n + 1):
+            if self.group[i]:
+                members_tested.append(i)
+
+
+class COMP(Algorithm):
+    '''Includes all items that do not appear in any negative test'''
+    def __init__(self, TestSet):
+        super().__init__(TestSet)
+
+    def test_group(self):
+        definitely_negative = []
+        product = False
+        for i in range(0, self.n + 1):
+            if self.group[i]:
+                product = product or self.test_array[i]
+                if product:
+                    break
+                else:
+                    definitely_negative.append(i)
+        if product:
+            definitely_negative = []
+
+
+test = TestSet(7, 3)
+print(test.test_array)
+pool = COMP(test)
