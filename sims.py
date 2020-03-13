27d51ae483ead33d7aef88b24cc2f905a1e7d59c
Ellen Brown <ellen.brown41@gmail.com>
2020-03-13 15:05:08 +0000

    Simulations functional for larger k

--- sims.py
+++ sims.py
@@ -1,7 +1,16 @@
 from group_testing import TestSet, COMP
+import numpy as np
 
-for n in range(10, 20):
+lower_bound = 10
+length_range = 10
+
+results = np.zeros(length_range)
+
+for n in range(lower_bound, length_range + lower_bound):
     # Floor division, makes 10% of set defective
     k = n // 10
     t = TestSet(n, k)
-    COMP(test)
+    noTests = COMP(t).T
+    results[n - lower_bound] = noTests
+
+print(results)
