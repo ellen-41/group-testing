c78b804df23299221fe89d36099efce0c42b1754
Ellen Brown <ellen.brown41@gmail.com>
2020-03-16 16:44:23 +0000

    Plot n vs T including equality line

--- sims.py
+++ sims.py
@@ -1,16 +1,26 @@
 from group_testing import TestSet, COMP
 import numpy as np
+import matplotlib.pyplot as plt
 
 lower_bound = 10
-length_range = 10
+length_range = 50
 
 results = np.zeros(length_range)
 
-for n in range(lower_bound, length_range + lower_bound):
+ns = range(lower_bound, length_range + lower_bound)
+
+for n in ns:
     # Floor division, makes 10% of set defective
     k = n // 10
     t = TestSet(n, k)
     noTests = COMP(t).T
     results[n - lower_bound] = noTests
 
-print(results)
+fig1 = plt.figure()
+ax1 = fig1.add_subplot(111)
+ax1.scatter(ns, results)
+ax1.set_xlabel("n")
+ax1.set_ylabel("num_tests")
+ax1.set_title("Size of test array against number of tests with k = n/10")
+ax1.plot(ns, ns)
+plt.show()
